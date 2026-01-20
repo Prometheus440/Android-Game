e8c990b96f3f3dcabb43cad2c6a9f83281816340
Matt Gilks <matthewgilks05@icloud.com>
2026-01-20 09:41:40 +0000

    All enemy scripts done

--- Assets/Scripts/Enemies/Fast Enemy.cs
+++ Assets/Scripts/Enemies/Fast Enemy.cs
@@ -1,59 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using Unity.Mathematics;
 using UnityEngine;
 
-public class fastEnemy : Enemy
+public class FastEnemy : Enemy
 {
-	private List<Node> path;
+    private List<Node> path;
+    private GameObject player;
 
-	private GameObject player;
-	private float pathUpdateInterval = 0.3f;
-	private float pathUpdateTimer = 0f;
+    private float pathUpdateInterval = 0.3f;
+    private float pathUpdateTimer = 0f;
+    private int lookAheadNodes = 3;
 
-	void Start()
-	{
-		health = 1;
-		movementSpeed = 5f;
+    void Start()
+    {
+        health = 1;
+        movementSpeed = 25f;
 
-		// Find the player
-		player = GameObject.FindGameObjectWithTag("Player");
-	}
+        // Find the player
+        player = GameObject.FindGameObjectWithTag("Player");
 
-	private void Update()
-	{
-		if (player != null && PathfindingGrid.Instance != null)
-		{
-			// Start timer
-			pathUpdateTimer += Time.deltaTime;
+        // So enemies don't update their path at the same time
+        pathUpdateTimer = UnityEngine.Random.Range(0f, pathUpdateInterval);
+    }
 
-			// If timer has reached 0.5f
-			if (pathUpdateTimer >= pathUpdateInterval)
-			{
-				// Check path
-				path = PathfindingGrid.Instance.RequestPath(transform.position, player.transform.position);
-				// Restart timer
-				pathUpdateTimer = 0f;
-			}
+    private void Update()
+    {
+        if (player != null && PathfindingGrid.Instance != null)
+        {
+            // Start timer
+            pathUpdateTimer += Time.deltaTime;
 
-			Move();
-		}
-	}
+            // If timer has reached 0.5f
+            if (pathUpdateTimer >= pathUpdateInterval)
+            {
+                // Check path
+                path = PathfindingGrid.Instance.RequestPath(transform.position, player.transform.position);
+                // Restart timer
+                pathUpdateTimer = 0f;
+            }
 
-	protected override void Move()
-	{
-		if (path != null && path.Count > 0)
-		{
-			Vector3 targetPos = path[0].nodePos;
-			Vector2 direction = new Vector2(targetPos.x - transform.position.x, targetPos.y - transform.position.y).normalized;
+            Move();
+        }
+    }
 
-			// Move
-			transform.position += new Vector3(direction.x, direction.y, 0) * movementSpeed * Time.deltaTime;
+    protected override void Move()
+    {
+        if (path != null && path.Count > 0)
+        {
+            Vector3 targetPos = FindCentre(path, lookAheadNodes);
 
-			if (Vector2.Distance(new Vector2(transform.position.x, transform.position.y), new Vector2(targetPos.x, targetPos.y)) < PathfindingGrid.Instance.nodeSize * 2)
-			{
-				path.RemoveAt(0);
-			}
-		}
-	}
+            // Smooth movement
+            Vector3 smoothPos = Vector3.Lerp(transform.position, targetPos, movementSpeed * Time.deltaTime);
+            transform.position = new Vector3(smoothPos.x, smoothPos.y, 0);
+
+            if (Vector2.Distance(new Vector2(transform.position.x, transform.position.y), new Vector2(targetPos.x, targetPos.y)) < PathfindingGrid.Instance.nodeSize * 2)
+            {
+                path.RemoveAt(0);
+            }
+        }
+    }
+
+    Vector3 FindCentre(List<Node> _path, int nodesToAverage)
+    {
+        int nodesToCheck = Mathf.Min(nodesToAverage, _path.Count);
+
+        float x = 0;
+        float y = 0;
+
+        for (int i = 0; i < nodesToCheck; i++)
+        {
+            x += _path[i].nodePos.x;
+            y += _path[i].nodePos.y;
+        }
+
+        x /= nodesToCheck;
+        y /= nodesToCheck;
+
+        return new Vector3(x, y, 0);
+    }
 }
