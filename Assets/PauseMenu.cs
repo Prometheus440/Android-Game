21a5742cb014d860ac4c9a2cbc29865cf622c271
Lawrex29 <joe.lawrex@gmail.com>
2026-01-27 10:05:48 +0000

    Fixed Settings menu

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
 
+    public GameObject pauseMenu;
+
     // Update is called once per frame
     void Update()
     {
@@ -16,15 +18,32 @@ public class PauseMenu : MonoBehaviour
     //Opens pause panel and stops game
     public void Pause()
     {
-        gameObject.SetActive(true);
+        pauseMenu.SetActive(true);
         Time.timeScale = 0;
     }
 
     //Closes pause panel and starts game
     public void Continue()
     {
-        gameObject.SetActive(false);
+        pauseMenu.SetActive(false);
         Time.timeScale = 1;
+    }
+
+    public void OpenSettings()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void CloseSettings()
+    {
+        StartCoroutine(DelayPause());
+    }
+
+    IEnumerator DelayPause()
+    {
+        yield return new WaitForSecondsRealtime(0.5f);
+
+        pauseMenu.SetActive(true);
     }
 
     public void Exit()
